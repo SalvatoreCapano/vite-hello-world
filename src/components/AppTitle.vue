5296425091001103661bebe7d303b4ce48636c25
Salvatore Capano <capano_salvatore@libero.it>
2023-02-01 18:00:23 +0100

    Esercizio Completato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+
+  <h1>La mia prima app con Vite!</h1>
+ 
+</template>
+
+<style scoped>
+h1 {
+  color: rgb(37, 100, 194);
+}
+</style>
