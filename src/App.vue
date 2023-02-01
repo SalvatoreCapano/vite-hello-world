f63fdc466410fc6047964f544af0eed132a8b7f4
Salvatore Capano <capano_salvatore@libero.it>
2023-02-01 18:19:44 +0100

    Bonus Completato

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-  import HelloWorld from './components/AppTitle.vue'
+  import AppTitle from './components/AppTitle.vue'
+  import AppHero from './components/AppHero.vue'
 </script>
 
 <template>
-  <HelloWorld/>
+  <AppHero/>
+  <AppTitle/>
 </template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <section>
+    <h2>Questo e' il Jumbotron</h2>
+  </section>
+ 
+</template>
+
+<style scoped>
+section {
+    height: 50vh;
+    background-color: gray;
+    background-image: url('https://picsum.photos/500/200');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+h2 {
+    line-height: 50vh;
+    text-align: center;
+    font-size: 3rem;
+    text-decoration: underline;
+}
+</style>
